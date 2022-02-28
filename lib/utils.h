b57b3edab1f63d0874505ea8a28a4f14b65582eb
SGIO <sebastien.giordano@cecielec.fr>
2022-02-28 15:03:21 +0100

    Add utils files.

---
+++ lib/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <cstdint>
+
+    uint16_t    swap_uint16( uint16_t val );
+    int16_t     swap_int16( int16_t val );
+    uint32_t    swap_uint32( uint32_t val );
+    int32_t     swap_int32( int32_t val );
+
+
+#endif
